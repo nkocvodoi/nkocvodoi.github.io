19352de958e9523ed3658de35c05bc61bded1b38
nkocvodoi <60961682+nkocvodoi@users.noreply.github.com>
2021-03-01 11:35:32 +0700

    update

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6cdf38abb5492661215fbd7c32f88bbc",
-"/": "6cdf38abb5492661215fbd7c32f88bbc",
-"main.dart.js": "3b7858fd19b20bf109519b299363c5a0",
+"index.html": "d5b152fcb52ef5d2938f96e09b68a1e7",
+"/": "d5b152fcb52ef5d2938f96e09b68a1e7",
+"main.dart.js": "ea27baa28b536ce10f678c5a8b80903a",
 "manifest.json": "9e7b34fd7c291ca2523123ef20497930",
 "version.json": "4b6db237b3514a88107a422469adfb0f"
 };
